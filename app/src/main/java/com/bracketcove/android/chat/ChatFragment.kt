e4a89565aeca8a1f7c01fc353dcc54290f7a79c6
Ryan <bracket.cove@gmail.com>
2023-02-25 18:25:09 -0700

    ChatFragment.kt ready for teaching

--- app/src/main/java/com/bracketcove/android/chat/ChatFragment.kt
+++ app/src/main/java/com/bracketcove/android/chat/ChatFragment.kt
@@ -21,30 +21,12 @@ import io.getstream.chat.android.ui.message.input.viewmodel.bindView
 import io.getstream.chat.android.ui.message.list.viewmodel.bindView
 import io.getstream.chat.android.ui.message.list.viewmodel.factory.MessageListViewModelFactory
 
-class ChatFragment : KeyedFragment(R.layout.fragment_chat) {
+class ChatFragment : KeyedFragment() {
 
     private val viewModel by lazy { lookup<ChatViewModel>() }
 
-    lateinit var binding: FragmentChatBinding
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        binding = FragmentChatBinding.bind(view)
+      //  super.onViewCreated(view, savedInstanceState)
 
-        binding.backIcon.setOnClickListener {
-            viewModel.handleBackButton()
-        }
-
-        val channelId: String = (getKey() as ChatKey).channelId
-        val messageListViewModel: MessageListViewModel by viewModels {
-            MessageListViewModelFactory(cid = channelId)
-        }
-
-        val messageInputViewModel: MessageInputViewModel by viewModels {
-            MessageListViewModelFactory(cid = channelId)
-        }
-
-        messageListViewModel.bindView(binding.messageListView, viewLifecycleOwner)
-        messageInputViewModel.bindView(binding.messageInputView, viewLifecycleOwner)
     }
 }
